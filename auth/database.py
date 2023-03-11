e6f73f7d62c31e689f5cc7b2af8f95ff3b19fe51
PolishBuska <375292765815@mail.ru>
2023-03-11 14:29:16 +0300

    init

--- auth/database.py
+++ auth/database.py
@@ -33,3 +33,4 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
+
